d25895f1a74ae4bacebe78e587e6bf65a5c26963
fee3fitri <safitri.shelton@gmail.com>
2022-10-07 10:33:06 -0700

    Adding map script in HTML for testing

---
+++ src/scripts/map.js
@@ -0,0 +1,33 @@
+google.charts.load('current', {
+  'packages':['geochart'],
+});
+google.charts.setOnLoadCallback(drawRegionsMap);
+
+var drawRegionsMap = () => {
+  var data = google.visualization.arrayToDataTable([
+    ['Country', 'Search volume/day'],
+    ['United States', 500000000],
+    ['Canada', 200000000],
+    ['United Kingdom', 250000000],
+    ['Ireland', 5000000],
+    ['South Africa', 300000000],
+    ['Nigeria', 3000000],
+    ['India', 500000000],
+    ['Songapore', 10000000],
+    ['Australia', 150000000],
+    ['New Zealand', 5000000],
+    ['Brazil', 450000000],
+    ['Russia', 100000000],
+    ['China', 250000000],
+    ['Indonesia', 425000000]
+  ]);
+
+  let options = {
+    colorAxis: {colors: ['#b2ccf7', '#2272f5']},
+    datalessRegionColor: '#ebf0f7'
+  };
+
+  var chart = new google.visualization.GeoChart(document.getElementById('regions_div'));
+
+  chart.draw(data, options);
+}
