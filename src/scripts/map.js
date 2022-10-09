15ebb418fd68d22617bf97a952376cbc2472cd9b
fee3fitri <safitri.shelton@gmail.com>
2022-10-08 22:30:06 -0700

    Add nav button

--- src/scripts/map.js
+++ src/scripts/map.js
@@ -1,95 +1,95 @@
 const COUNTRIES = [
-  {"United States": {
-    'Broncos': 1000000,
-    'Omonia vs Man United': 500000,
-    'Pixel 7': 200000,
-    'Mario movie trailer': 200000,
-    'Arsenal vs bodø/glimt': 200000
-  }},
-  {"Canada": {
-    'Omonia vs Man United': 100000,
-    'UCP leadership race': 50000,
-    'Pixel 7': 50000,
-    'Thailand': 20000,
-    'Arsenal vs bodø/glimt': 20000
-  }},
-  {"United Kingdom": {
-    'Arsenal vs bodø/glimt': 500000,
-    'Velma': 200000,
-    'Anderlecht vs West Ham': 100000,
-    'Pixel 7': 50000,
-    'Thailand': 50000
-  }},
-  {"Ireland": {
-    'Arsenal vs bodø/glimt': 20000,
-    'Velma': 10000,
-    'Shamrock Rovers': 10000,
-    'Irish dancing scandal': 5000,
-    'Anderlecht vs West Ham': 5000
-  }},
-  {"South Africa": {
-    'Arsenal vs bodø/glimt': 100000,
-    'Velma': 20000,
-    'Roma vs Real Betis': 20000,
-    'Man Utd': 20000,
-    'Anderlecht vs West Ham': 20000
-  }},
-  {"Nigeria": {
-    'Chelsea vs Milan': 200000,
-    'Roma vs Real Betis': 20000,
-    'Man United': 20000,
-    'Anderlecht vs West Ham': 10000,
-    'Sheriff vs Real Sociedad': 10000
-  }},
-  {"India": {
-    'Cricket Live': 1000000,
-    'Man City': 100000,
-    'Arsenal vs bodø/glimt': 100000,
-    'Pixel 7': 100000,
-    'Real Madrid': 50000
-  }},
-  {"Singapore": {
-    'Real Madrid': 20000,
-    'Man City': 10000,
-    'Real Madrid vs Shakhtar Donetsk': 10000,
-    'Pixel 7': 10000,
-    'Thailand': 10000
-  }},
-  {"Australia": {
-    'Omonia vs Man United': 100000,
-    'Chelsea vs Milan': 50000,
-    'Powerball Results': 50000,
-    'Thailand': 50000,
-    'Benfica vs PSG': 50000
-  }},
-  {"New Zealand": {
-    'Omonia vs Man United': 20000,
-    'Arsenal vs bodø/glimt': 10000,
-    'Thailand': 5000,
-    'Peter Ellis': 2000,
-    'Megaland': 2000
-  }},
-  {"Brazil": {
-    'Alexandre de Moraes': 200000,
-    'Jogo de Palmeiras': 100000,
-    'Noruega x Brasil': 100000,
-    'Fernanda Montenegro': 100000,
-    'Valencia': 100000
-  }},
-  {"Russia": {
-    'Паша Техник': 100000,
-    'Кот': 20000,
-    'Байден': 20000,
-    'Мемориал': 20000,
-    'Никита Пресняков': 20000
-  }},
-  {"Indonesia": {
-    'Maulid Nabi 2022': 200000,
-    'Arsenal': 50000,
-    'Agama Farel Prayoga': 50000,
-    'Dadang Aremania': 50000,
-    'BMKG': 20000
-  }}
+  {"United States": [
+    {'trends': 'Broncos', 'volume': 1000000},
+    {'trends': 'Omonia vs Man United', 'volume': 500000},
+    {'trends': 'Pixel 7', 'volume': 200000},
+    {'trends': 'Mario movie trailer', 'volume': 200000},
+    {'trends': 'Arsenal vs bodø/glimt', 'volume': 200000}
+]},
+  {"Canada": [
+    {'trends': 'Omonia vs Man United', 'volume': 100000},
+    {'trends': 'UCP leadership race', 'volume': 50000},
+    {'trends': 'Pixel 7', 'volume': 50000},
+    {'trends': 'Thailand', 'volume': 20000},
+    {'trends': 'Arsenal vs bodø/glimt', 'volume': 20000},
+  ]},
+  {"United Kingdom": [
+    {'trends': 'Arsenal vs bodø/glimt', 'volume': 500000},
+    {'trends': 'Velma', 'volume': 200000},
+    {'trends': 'Anderlecht vs West Ham', 'volume': 100000},
+    {'trends': 'Pixel 7', 'volume': 50000},
+    {'trends': 'Thailand', 'volume': 50000},
+  ]},
+  {"Ireland": [
+    {'trends': 'Arsenal vs bodø/glimt', 'volume': 20000},
+    {'trends': 'Velma', 'volume': 10000},
+    {'trends': 'Shamrock Rovers', 'volume': 10000},
+    {'trends': 'Irish dancing scandal', 'volume': 5000},
+    {'trends': 'Anderlecht vs West Ham', 'volume': 5000},
+  ]},
+  {"South Africa": [
+    {'trends': 'Arsenal vs bodø/glimt', 'volume': 100000},
+    {'trends': 'Velma', 'volume': 20000},
+    {'trends': 'Roma vs Real Betis', 'volume': 20000},
+    {'trends': 'Man Utd', 'volume': 20000},
+    {'trends': 'Anderlecht vs West Ham', 'volume': 20000},
+  ]},
+  {"Nigeria": [
+    {'trends': 'Chelsea vs Milan', 'volume': 200000},
+    {'trends': 'Roma vs Real Betis', 'volume': 20000},
+    {'trends': 'Man United', 'volume': 20000},
+    {'trends': 'Anderlecht vs West Ham', 'volume': 10000},
+    {'trends': 'Sheriff vs Real Sociedad', 'volume': 10000},
+  ]},
+  {"India": [
+    {'trends': 'Cricket Live', 'volume': 1000000},
+    {'trends': 'Man City', 'volume': 100000},
+    {'trends': 'Arsenal vs bodø/glimt', 'volume': 100000},
+    {'trends': 'Pixel 7', 'volume': 100000},
+    {'trends': 'Real Madrid', 'volume': 50000},
+  ]},
+  {"Singapore": [
+    {'trends': 'Real Madrid', 'volume': 20000},
+    {'trends': 'Man City', 'volume': 10000},
+    {'trends': 'Real Madrid vs Shakhtar Donetsk', 'volume': 10000},
+    {'trends': 'Pixel 7', 'volume': 10000},
+    {'trends': 'Thailand', 'volume': 10000},
+  ]},
+  {"Australia": [
+    {'trends': 'Omonia vs Man United', 'volume': 100000},
+    {'trends': 'Chelsea vs Milan', 'volume': 50000},
+    {'trends': 'Powerball Results', 'volume': 50000},
+    {'trends': 'Thailand', 'volume': 50000},
+    {'trends': 'Benfica vs PSG', 'volume': 50000},
+  ]},
+  {"New Zealand": [
+    {'trends': 'Omonia vs Man United', 'volume': 20000},
+    {'trends': 'Arsenal vs bodø/glimt', 'volume': 10000},
+    {'trends': 'Thailand', 'volume': 5000},
+    {'trends': 'Peter Ellis', 'volume': 2000},
+    {'trends': 'Megaland', 'volume': 2000},
+  ]},
+  {"Brazil": [
+    {'trends': 'Alexandre de Moraes', 'volume': 200000},
+    {'trends': 'Jogo de Palmeiras', 'volume': 100000},
+    {'trends': 'Noruega x Brasil', 'volume': 100000},
+    {'trends': 'Fernanda Montenegro', 'volume': 100000},
+    {'trends': 'Valencia', 'volume': 100000},
+  ]},
+  {"Russia": [
+    {'trends': 'Паша Техник', 'volume': 100000},
+    {'trends': 'Кот', 'volume': 20000},
+    {'trends': 'Байден', 'volume': 20000},
+    {'trends': 'Мемориал', 'volume': 20000},
+    {'trends': 'Никита Пресняков', 'volume': 20000},
+  ]},
+  {"Indonesia": [
+    {'trends': 'Maulid Nabi 2022', 'volume': 200000},
+    {'trends': 'Arsenal', 'volume': 50000},
+    {'trends': 'Agama Farel Prayoga', 'volume': 50000},
+    {'trends': 'Dadang Aremania', 'volume': 50000},
+    {'trends': 'BMKG', 'volume': 20000},
+  ]}
 ];
 
 export default class Map {
@@ -116,35 +116,34 @@ export default class Map {
   
     const data = response.getDataTable();
     const chart = new google.visualization.GeoChart(document.getElementById('regions_div'));
-    
     const options = {
       colorAxis: {colors: ['#b2ccf7', '#2272f5']},
       datalessRegionColor: '#ebf0f7',
       backgroundColor: ''
     };
-    
-    function clickHandler() {
+
+    const clickHandler = () => {
       const selection = chart.getSelection();
       const trendBox = document.querySelector(".trend-box");
-      let country, objVal, countryTrends;
-      
+      let country, trendsObj, countryTrends,countryNode;
   
       for (let item of selection) {
         const row = item.row;
         country = data.getValue(row, 0);
-        objVal = Object.values(COUNTRIES[row]);
-        countryTrends = Object.values(objVal)[0];
-      }
-  
-      // if (country === '') country = 'nothing';
-      trendBox.innerHTML = `<h3>${country}</h3><p>${countryTrends}</p>`;
-      console.log(countryTrends);
-    }
-
+        trendsObj = Object.values((COUNTRIES[row]))[0];
+      };
+      
+      trendBox.innerHTML = `
+      <div class="trend-content"><p>${Object.values(trendsObj[0])}</p></div>
+      <div class="trend-content"><p>${Object.values(trendsObj[1])}</p></div>
+      <div class="trend-content"><p>${Object.values(trendsObj[2])}</p></div>
+      <div class="trend-content"><p>${Object.values(trendsObj[3])}</p></div>
+      <div class="trend-content"><p>${Object.values(trendsObj[4])}</p></div>`
+    };
+    
     chart.draw(data, options);
     google.visualization.events.addListener(chart, 'select', clickHandler);
   }
-  
 
   // Show the map when the page is loaded
   load() {
