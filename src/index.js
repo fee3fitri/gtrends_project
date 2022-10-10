93dcd446b02f1d774beb6deaad29d331461e4f6d
fee3fitri <safitri.shelton@gmail.com>
2022-10-10 12:43:34 -0700

    Change style to the slider

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import Map from "./scripts/map"
 import Trends from "./scripts/trends"
 import Swiper from '../node_modules/swiper/swiper-bundle.esm';
-import 'swiper/css';
-import 'swiper/css/pagination';
 
 document.addEventListener("DOMContentLoaded", () => {
   const mapContainer = document.getElementById("regions_div");
